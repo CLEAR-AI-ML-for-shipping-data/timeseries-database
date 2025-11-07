2011edc7fe6418f7f5505afe4d097e23a54c794d
Leon Boschman <leon.boschman@chalmers.se>
2025-11-07 13:01:08 +0100

    Add creation of dwh tables

---
+++ sql_snippets/10_create_dwh_tables.sql
@@ -0,0 +1,21 @@
+create schema if not exists dwh;
+
+drop table if exists dwh.ships cascade;
+
+CREATE TABLE dwh.ships (
+        id SERIAL NOT NULL,
+        mmsi VARCHAR(20) NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE (mmsi)
+);
+
+drop table if exists dwh.positions cascade;
+CREATE TABLE dwh.positions (
+        position_id SERIAL NOT NULL,
+        gps_position geometry(POINT,4326) NOT NULL,
+        ship_id INTEGER,
+        gps_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        nav_status VARCHAR NOT NULL,
+        PRIMARY KEY (position_id),
+        FOREIGN KEY(ship_id) REFERENCES ships (id)
+);
