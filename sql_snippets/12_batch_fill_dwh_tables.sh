a22ac6b5bfbf8996a920a8d5d2679ba8a38a0757
Leon Boschman <leon.boschman@chalmers.se>
2025-11-14 14:08:10 +0100

    Add better argument handling to batch_fill_dwh_tables.sh

--- sql_snippets/12_batch_fill_dwh_tables.sh
+++ sql_snippets/12_batch_fill_dwh_tables.sh
@@ -1,14 +1,51 @@
-DB_CONN=$1
+usage() {
+  cat <<EOF
+Usage: ${0##*/} [-h] [-b BATCH_SIZE] [-s STAGETABLE] -d DATABASE 
+Connect to database DATABASE and run data insertion.
+
+Available options:
+
+     -b      Batch size, default is 1_000_000
+     -d      Database connection string
+     -h      Show this help
+     -s      The staging table to which the data will be copied. The default is "stg.csv_data"
+EOF
+}
 
 STAGETABLE="stg.csv_data"
 BATCH_SIZE=1000000
+DB_CONN=""
+
+while getopts "b:d:hs:" opt; do
+  case $opt in
+  b)
+    BATCH_SIZE=${OPTNAME}
+    ;;
+  d)
+    DB_CONN=${OPTNAME}
+    ;;
+  h)
+    usage
+    exit 0
+    ;;
+  s)
+    STAGETABLE=${OPTNAME}
+    ;;
+  *)
+    echo "Invalid option ${opt}"
+    ;;
+  esac
+done
+
+if [[ "${DB_CONN}" == "" ]]; then
+  echo "Empty database string"
+  usage >&2
+fi
 
 echo "Counting rows of $STAGETABLE, this might take a while..."
 ROWCOUNT=$(
-  psql "${DB_CONN}" -c "SELECT COUNT(*) FROM ${STAGETABLE}" --csv -t
+  psql --dbname"${DB_CONN}" -c "SELECT COUNT(*) FROM ${STAGETABLE}" --csv -t
 )
-
-# ROWCOUNT=1197729565
 
 echo "Total number of rows: ${ROWCOUNT}"
 
@@ -17,18 +54,10 @@ COUNTER=0
 
 echo "Inserting $TOTAL_BATCHES batches of $BATCH_SIZE rows..."
 
-# for STARTROW in $(seq 1 $BATCH_SIZE $ROWCOUNT); do
-# for STARTROW in 1..$BATCH_SIZE..$ROWCOUNT; do
-# for STARTROW in {1..${ROWCOUNT}..${BATCH_SIZE}}; do
 for ((BATCH = 0; $BATCH < $TOTAL_BATCHES; BATCH++)); do
-  # ((COUNTER+=1))
   ((COUNTER = BATCH + 1))
   ((STARTROW = BATCH * BATCH_SIZE + 1))
   ((ENDROW = COUNTER * BATCH_SIZE))
   echo "Inserting batch $COUNTER / $TOTAL_BATCHES (rows $STARTROW - $ENDROW)"
-  psql "${DB_CONN}" -c "\timing on" -c "CALL dwh.insert_into_dwh( $STARTROW, $ENDROW );"
+  psql --dbname="${DB_CONN}" -c "\timing on" -c "CALL dwh.insert_into_dwh( $STARTROW, $ENDROW );"
 done
-
-# for ((i = 0; i < 10; i++)); do
-#   echo "$i"
-# done
