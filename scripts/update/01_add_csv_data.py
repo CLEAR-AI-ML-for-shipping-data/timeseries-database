68cf3314fa6b199c52c3a6363fb55e3bd8558c3b
Leon Boschman <leon.boschman@chalmers.se>
2025-11-19 21:12:00 +0100

    Add multiprocessing for concurrent uploading to database
    This leads to a 2x speedup with limited data (5 files instead of one)

--- scripts/update/01_add_csv_data.py
+++ scripts/update/01_add_csv_data.py
@@ -1,7 +1,12 @@
 from argparse import ArgumentParser
+from itertools import repeat
+from multiprocessing import Pool
+from pathlib import Path
+from typing import Tuple, Union
 
 import pandas as pd
 import psycopg
+from tqdm import tqdm
 
 CSVCOLS = [
     "Base station time stamp",
@@ -30,7 +35,7 @@ rename_cols = {ccol: tcol for ccol, tcol in zip(CSVCOLS, TABLECOLS, strict=True)
 COPY_STATEMENT = f"COPY stg.csv_data ( {','.join(TABLECOLS)} ) FROM STDIN CSV"
 
 
-def main(dbname: str, csv_file: str):
+def upload_csv_to_db(dbname: str, csv_file: Union[str, Path]):
     conn = psycopg.connect(dbname)
     df = pd.read_csv(csv_file, usecols=CSVCOLS)
 
@@ -40,12 +45,43 @@ def main(dbname: str, csv_file: str):
                 copy.write(df.to_csv(header=False, index=False))
 
 
+def star_upload_csv_to_db(arg: Tuple):
+    return upload_csv_to_db(*arg)
+
+
+def get_csv_paths(folder: str):
+    datafolder = Path(folder)
+    csv_files = []
+    for item in datafolder.walk():
+        csv_files += [
+            item[0] / datafile
+            for datafile in item[2]
+            if datafile[-4:].lower() == ".csv"
+        ]
+    return csv_files
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Add CSV data")
 
     parser.add_argument("-d", "--dbname", type=str, required=True)
-    parser.add_argument("-c", "--csv_file", type=str, required=True)
+
+    source_group = parser.add_mutually_exclusive_group(required=True)
+    source_group.add_argument("-c", "--csv_file", type=str)
+    source_group.add_argument("-f", "--folder", type=str)
+
+    parser.add_argument("-n", "--nworkers", type=int, default=4)
 
     args = parser.parse_args()
+    dbname = args.dbname
 
-    main(dbname=args.dbname, csv_file=args.csv_file)
+    if (csv_file := args.csv_file) is not None:
+        upload_csv_to_db(dbname=dbname, csv_file=csv_file)
+
+    if (folder := args.folder) is not None:
+        csv_files = get_csv_paths(folder)
+        input_args = list(zip(repeat(dbname), csv_files))
+        with Pool(args.nworkers) as p:
+            r = list(
+                tqdm(p.imap(star_upload_csv_to_db, input_args), total=len(input_args))
+            )
