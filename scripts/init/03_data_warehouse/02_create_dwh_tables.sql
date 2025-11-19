5b98a4a3aeb15900bf889d6887c4b92ec2d1d23f
Leon Boschman <leon.boschman@chalmers.se>
2025-11-19 11:20:02 +0100

    Split script for creating dwh schema and dwh tables

--- scripts/init/03_data_warehouse/02_create_dwh_tables.sql
+++ scripts/init/03_data_warehouse/02_create_dwh_tables.sql
@@ -1,41 +1,29 @@
-CREATE schema IF NOT EXISTS dwh;
-
-CREATE extension IF NOT EXISTS postgis;
-CREATE extension if not exists timescaledb;
-
-DROP TABLE IF EXISTS dwh.ships CASCADE;
-
-CREATE TABLE dwh.ships (
+CREATE TABLE IF NOT EXISTS dwh.ships (
     id SERIAL NOT NULL,
     mmsi VARCHAR(20) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (mmsi)
 );
 
-DROP TABLE IF EXISTS dwh.nav_statuses CASCADE;
-
-CREATE TABLE dwh.nav_statuses (
+CREATE TABLE IF NOT EXISTS dwh.nav_statuses (
     id serial NOT NULL,
     nav_status varchar NOT NULL,
     PRIMARY KEY(id),
     UNIQUE (nav_status)
 );
 
-DROP TABLE IF EXISTS dwh.positions CASCADE;
-
 CREATE TABLE dwh.positions (
     position_id SERIAL NOT NULL,
     gps_position geometry(POINT, 4326) NOT NULL,
     ship_id INTEGER,
-    -- gps_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     gps_timestamp TIMESTAMPTZ,
     nav_status_id integer,
     speed_over_ground float,
     heading float,
     course_over_ground float,
-    -- PRIMARY KEY (position_id),
     FOREIGN KEY(ship_id) REFERENCES dwh.ships (id),
     FOREIGN KEY(nav_status_id) REFERENCES dwh.nav_statuses (id)
 );
 
-SELECT create_hypertable('dwh.positions', 'gps_timestamp', 'ship_id', 50);
+SELECT
+    create_hypertable('dwh.positions', 'gps_timestamp', 'ship_id', 50);
